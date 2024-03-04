0f8636c24b99c153db99d75b380c097bcb7da760
ramrajsainiiitd <109168257+ramrajsainiiitd@users.noreply.github.com>
2024-03-04 11:52:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function SecondHighELement(arr){
+   for(i=0;i<2;i++){
+    for(j=0;j<arr.length-i;j++){
+
+       if(arr[j]>arr[j+1]){
+        temp=arr[j];
+        arr[j]= arr[j+1];
+        arr[j+1]=temp;
+       } 
+    }
+   }
+   return arr[arr.length-2];
+
+}
+console.log(SecondHighELement(arr));
